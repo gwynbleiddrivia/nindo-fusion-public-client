7ddd4f51a97cb15f5f24f61fd801784c14e46e30
gwynbleiddrivia <yasinzme2@gmail.com>
2023-06-11 13:20:50 +0600

    event handler added in login and register

--- src/authentication/Login.jsx
+++ src/authentication/Login.jsx
@@ -1,11 +1,20 @@
 import {useState} from 'react'
 import { FaGoogle } from 'react-icons/fa';
+import { Link } from 'react-router-dom'
 
 const Login = () => {
 	let [isHide,setIsHide] = useState(false)
 	const handleHide = () =>{
 		setIsHide(!isHide)
 	}
+	
+	const handleLogin = event =>{
+		event.preventDefault()
+		const email = event.target.email.value
+		const password = event.target.password.value
+		console.log(email, password)
+	}
+
 	return (
 		<div className="text-white">
 			<div className="hero backdrop-blur w-fit mx-auto my-2 rounded-xl">
@@ -14,12 +23,14 @@ const Login = () => {
 			      <h1 className="text-5xl font-xl">Login From Here</h1>
 			    </div>
 			    <div className="card flex-shrink-0 w-full max-w-sm">
-			      <div className="card-body">
+			      <form onSubmit={handleLogin} className="card-body">
+			
+				
 				<div className="form-control">
 				  <label className="label">
 				    <span className="label-text text-white">Email</span>
 				  </label>
-				  <input type="text" placeholder="email" className="input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" />
+				  <input name="email" type="email" placeholder="email" className="input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required/>
 				</div>
 				<div className="form-control">
 				  <label className="label">
@@ -27,18 +38,18 @@ const Login = () => {
 				  </label>
 				  { 	isHide?
 				  <>
-				  	<input type="password" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" />
+				  	<input name="password" type="password" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required />
 				  	<button onClick={handleHide}><small className="text-amber-400">Click here to SHOW password</small></button>
 				  </>:
 				  <>
-				  <input type= "text" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" />
+				  <input name="password" type="text" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required/>
 					<button onClick={handleHide}><small className="text-amber-400">Click here to HIDE password</small></button>
 				  </>
 				  }
 				  <label className="label">
 				  </label>
 				</div>
-				<div className="form-control mt-6">
+				<div className="form-control mt-2">
 				  <button className="btn bg-[rgb(98,81,48)] text-black hover:text-white">Login</button>
 				  <button className="mt-5 btn bg-[rgb(98,81,48)] text-black hover:text-white">
 				  <div className="flex justify-around w-full">
@@ -48,8 +59,12 @@ const Login = () => {
 				  </div>
 				 
 				  </button>
+				  <Link to="/register" className="font-bold mt-2 text-center">
+				  <small>Does not have an account yet? Register here</small>
+
+				  </Link>
 				</div>
-			      </div>
+			      </form>
 			    </div>
 			  </div>
 			</div>
--- src/authentication/Register.jsx
+++ src/authentication/Register.jsx
@@ -1,8 +1,95 @@
-
+import {useState} from 'react'
+import { FaGoogle } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 const Register = () => {
+	let [isHide,setIsHide] = useState(false)
+	const handleHide = () =>{
+		setIsHide(!isHide)
+	}
+	
+	const handleRegister = event =>{
+		event.preventDefault()
+		const username = event.target.username.value
+		const userphoto = event.target.userphoto.value
+		const email = event.target.email.value
+		const password = event.target.password.value
+		const confirmpass = event.target.confirmpass.value
+		console.log(email,password)
+	}
+
 	return (
-		<div>
-			<h2>This is Register</h2>
+		<div className="text-white">
+			<div className="hero backdrop-blur w-fit mx-auto my-2 rounded-xl">
+			  <div className="hero-content flex-col">
+			    <div className="text-center lg:text-left">
+			      <h1 className="text-5xl font-xl">Register From Here</h1>
+			    </div>
+			    <div className="card flex-shrink-0 w-full max-w-sm">
+			      <form onSubmit={handleRegister} className="card-body">
+				<div className="flex gap-2 mx-auto">
+				<div className="form-control">
+				  <label className="label">
+				    <span className="label-text text-white">Name</span>
+				  </label>
+				  <input name="username" type="text" placeholder="your name" className="input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white"/>
+				</div>
+				<div className="form-control">
+				  <label className="label">
+				    <span className="label-text text-white">Photo URL</span>
+				  </label>
+				  <input name="userphoto" type="text" placeholder="your photo url" className="input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white"/>
+				</div>
+				
+				</div>
+
+
+				<div className="form-control">
+				  <label className="label">
+				    <span className="label-text text-white">Email</span>
+				  </label>
+				  <input name="email" type="email" placeholder="email" className="input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required/>
+				</div>
+				<div className="form-control">
+				  <label className="label">
+				  </label>
+				  { 	isHide?
+				  <>
+				    <span className="my-2 label-text text-white">Password</span>
+				  	<input name="password" type="password" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required />
+				    <span className="my-2 label-text text-white">Confirm Password</span>
+				  	<input name="confirmpass" type="password" placeholder="confirm password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required />
+				  	<button onClick={handleHide}><small className="text-amber-400">Click here to SHOW password</small></button>
+				  </>:
+				  <>
+				    <span className="my-2 label-text text-white">Password</span>
+				  <input name="password" type="text" placeholder="password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required/>
+				    <span className="my-2 label-text text-white">Confirm Password</span>
+				  <input name="confirmpass" type= "text" placeholder="confirm password" className="mb-3 input input-ghost input-bordered text-white active:bg-transparent hover:bg-transparent focus:bg-transparent fill-transparent focus:text-white border border-white" required/>
+					<button onClick={handleHide}><small className="text-amber-400">Click here to HIDE password</small></button>
+				  </>
+				  }
+				  <label className="label">
+				  </label>
+				</div>
+				<div className="form-control mt-2">
+				  <button className="btn bg-[rgb(98,81,48)] text-black hover:text-white">Register</button>
+				  <button className="mt-5 btn bg-[rgb(98,81,48)] text-black hover:text-white">
+				  <div className="flex justify-around w-full">
+				   Register from Google
+				  <FaGoogle/>
+				  
+				  </div>
+				 
+				  </button>
+				  <Link to="/register" className="font-bold mt-2 text-center">
+				  <small>Does not have an account yet? Register here</small>
+
+				  </Link>
+				</div>
+			      </form>
+			    </div>
+			  </div>
+			</div>
 
 		</div>
 	);
